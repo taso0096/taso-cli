458d25abd50959c6c6eee0a48ecf56ce0799b184
taso0096 <jin2931121@yahoo.co.jp>
2021-08-03 20:28:58 +0900

    [Update] rootDirにリポジトリ追加機能実装

--- src/models/tasoCli.ts
+++ src/models/tasoCli.ts
@@ -3,12 +3,25 @@ interface Result {
   data: null | string;
 }
 
+interface DirObject {
+  [key: string]: any;
+}
+interface GitHubFile {
+  path: string;
+  type: string;
+  sha: string;
+}
+type GitHubDir = GitHubFile[];
+
 export class TasoCli {
   user: string;
-  rootDir: any;
+  rootDir: DirObject;
   cd: string;
   history: string[];
   result: Result[];
+
+  repo: string;
+  allowGetRepo: boolean;
 
   constructor() {
     this.user = 'taso0096';
@@ -16,15 +29,21 @@ export class TasoCli {
     this.cd = `/home/${this.user}`;
     this.history = [];
     this.result = [];
+
+    this.repo = 'taso-cli';
+    this.allowGetRepo = false;
   }
 
-  boot(): void {
-    this.rootDir = {
-      home: {}
-    };
+  async boot(): Promise<void> {
+    this.rootDir.home = {};
     this.rootDir.home[this.user] = {
       repositories: {}
     };
+
+    if (this.allowGetRepo) {
+      const repoDir = await this._getRepoDir();
+      this.rootDir.home[this.user].repositories[this.repo] = repoDir;
+    }
   }
 
   execCmd(cmd: string): void {
@@ -34,5 +53,41 @@ export class TasoCli {
     };
     this.history.push(cmd);
     this.result.push(result);
+  }
+
+  async _getRepoDir(): Promise<DirObject> {
+    const repoDir = {};
+    const commitSha: null | string = await fetch(`https://api.github.com/repos/${this.user}/${this.repo}/commits`)
+      .then(res => {
+        if (res.status !== 200) {
+          throw new Error();
+        }
+        return res.json();
+      })
+      .then(json => json[0].sha)
+      .catch(() => null);
+
+    if (commitSha) {
+      await this._getDir(repoDir, commitSha);
+    }
+    return repoDir;
+  }
+
+  async _getDir(parentDir: DirObject, sha: string): Promise<void> {
+    const dir: GitHubDir = await this._getTree(sha);
+    for (const file of dir) {
+      const { path, type, sha } = file;
+      parentDir[path] = type === 'tree' ? {} : true;
+      if (type === 'tree') {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        await this._getDir(parentDir[path], sha);
+      }
+    }
+  }
+
+  async _getTree(sha: string): Promise<GitHubDir> {
+    return fetch(`https://api.github.com/repos/${this.user}/${this.repo}/git/trees/${sha}`)
+      .then(res => res.json())
+      .then(json => json.tree);
   }
 }
