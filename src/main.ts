8ebed768b999f1ba8106eb2755848dcd40abf0c5
taso0096 <jin2931121@yahoo.co.jp>
2021-08-12 22:50:14 +0900

    [Chore] firebaseインストールと設定

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
+import firebase from 'firebase/app';
+import firebaseConfig from './firebaseConfig';
+
 import '@/assets/sass/main.scss';
+
+firebase.initializeApp(firebaseConfig);
 
 createApp(App).use(router).mount('#app');
