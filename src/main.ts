a4ad4fea26ba8965d4a90bffec1abc06d82e56d5
taso0096 <jin2931121@yahoo.co.jp>
2021-08-20 18:17:35 +0900

    [Chore] GoogleAnalytics設定追加

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,12 @@ import './registerServiceWorker';
 import router from './router';
 
 import firebase from 'firebase/app';
+import 'firebase/analytics';
 import firebaseConfig from './firebaseConfig';
 
 import '@/assets/sass/main.scss';
 
 firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 createApp(App).use(router).mount('#app');
